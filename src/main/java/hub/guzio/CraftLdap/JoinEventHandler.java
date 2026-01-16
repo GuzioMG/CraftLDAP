5e6ddcc69235eca38bdadaeb94bb51dafee20c3d
Guzio <guzekk@protonmail.com>
2026-01-17 00:45:14 +0100

    Implemented pretty much the entire logic.
    Most of it is not yet tested (I only got as far as "getting rejected due to an earlier LDAP connection error" or "safe-mode off; everyone in"), because I don't have an LDAP server on my local machine and I don't really feel like configuring one from scratch. Will upload it onto GuzioHub tomorrow, to test with in a real LDAP env.

---
+++ src/main/java/hub/guzio/CraftLdap/JoinEventHandler.java
@@ -0,0 +1,83 @@
+package hub.guzio.CraftLdap;
+
+import com.unboundid.ldap.sdk.*;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.network.DisconnectionInfo;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+import org.jspecify.annotations.NonNull;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+import java.util.UUID;
+
+public class JoinEventHandler implements ServerPlayConnectionEvents.Init {
+
+    private final LDAPConnection connection;
+    private final Properties config;
+
+    JoinEventHandler(LDAPConnection connection, Properties config){
+        this.connection = connection;
+        this.config = config;
+    }
+
+    @Override
+    public void onPlayInit(@NotNull ServerPlayNetworkHandler event, @NonNull MinecraftServer server) {
+        DisconnectionInfo errmsg_ldap = new DisconnectionInfo(Text.literal(config.getProperty(Main.KEY_ERRUNKNOWN)));
+        try {
+            errmsg_ldap = new DisconnectionInfo(errmsg_ldap.reason(), Optional.empty(), Optional.of(new URI(config.getProperty(Main.KEY_WEBSITE))));
+        } catch (URISyntaxException e) {
+            Main.wrn.log("Couldn't parse bugreport URI, will not provide it at all. Error details:", e);
+        }
+
+        if (connection == null){
+            Main.wrn.log(event.player.getName().getString() + " attempted to join, but CraftLDAP couldn't connect to LDAP and it's set to „fail-closed”. Their join attempt will get automatically rejected.");
+            event.disconnect(errmsg_ldap);
+            return;
+        }
+
+        try {
+            var results = filterEntries(event.getPlayer().getUuid(), connection.search(config.getProperty(Main.KEY_GROUP), SearchScope.SUB, Main.KEY_GROUP).getSearchEntries(), config.getProperty(Main.KEY_MCUUID), config.getProperty(Main.KEY_LDAPNAME));
+            if (results.length < 1){
+                Main.out.log(event.player.getName().getString() + " attempted to join, but was not authenticated with LDAP.");
+                event.disconnect(new DisconnectionInfo(Text.literal(config.getProperty(Main.KEY_ERRAUTH))));
+            }
+            else {
+                Main.out.log(event.player.getName().getString() + " joined as the following LDAP user(s): [ \""+String.join("\", \"", results)+"\" ].");
+            }
+        }
+        catch (LDAPException e) {
+            Main.err.log(event.player.getName().getString() + " attempted to join, but CraftLDAP couldn't determine whether they're authorized to do so (so their join attempt was rejected), due to the following error:", e);
+            event.disconnect(errmsg_ldap);
+        }
+    }
+
+    public static String[] filterEntries(UUID player, List<SearchResultEntry> results, String uuidKey, String nameKey) throws LDAPException{
+        var outputRaw = new String[results.size()];
+        int index = 0;
+
+        try{
+            for (var result : results) {
+                if (result.getAttribute(uuidKey).getValue().equals(player.toString())){
+                    outputRaw[index] = result.getAttribute(nameKey).getValue();
+                    index++;
+                }
+            }
+        }
+        catch (NullPointerException e){
+            throw new LDAPException(ResultCode.NO_SUCH_ATTRIBUTE, e);
+        }
+
+        var output = new String[index];
+        while (index > 0){
+            output[index-1] = outputRaw[index-1];
+            index--;
+        }
+        return output;
+    }
+}
--- src/main/java/hub/guzio/CraftLdap/Main.java
+++ src/main/java/hub/guzio/CraftLdap/Main.java
@@ -1,11 +1,123 @@
 package hub.guzio.CraftLdap;
 
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.*;
+import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggingEventBuilder;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Properties;
 
 public class Main implements ModInitializer {
 
+    public static final LoggingEventBuilder out = LoggerFactory.getLogger("CraftLDAP").atInfo();
+    public static final LoggingEventBuilder err = LoggerFactory.getLogger("CraftLDAP").atError();
+    public static final LoggingEventBuilder wrn = LoggerFactory.getLogger("CraftLDAP").atWarn();
+    public static final LoggingEventBuilder dbg = LoggerFactory.getLogger("CraftLDAP").atDebug();
+
+    public static final String KEY_HOST = "host";
+    public static final String KEY_PORT = "port";
+    public static final String KEY_USER = "bind-dn";
+    public static final String KEY_PASS = "bind-pass";
+    public static final String KEY_SAFE = "safe-mode";
+    public static final String KEY_GROUP = "base-dn";
+    public static final String KEY_FILTER = "filter";
+    public static final String KEY_WEBSITE = "bugreport-url";
+    public static final String KEY_LDAPNAME = "ldap-attr-name";
+    public static final String KEY_MCUUID = "ldap-attr-mcuuid";
+    public static final String KEY_ERRUNKNOWN = "message-error";
+    public static final String KEY_ERRAUTH = "message-unauthorized";
+
     @Override
     public void onInitialize() {
-        System.out.println("TEST!!!");
+        out.log("Starting CraftLDAP...");
+
+        out.log("Getting the config...");
+        Path props = FabricLoader.getInstance().getConfigDir().resolve("ldap.properties");
+        int config_port = 3890;
+        dbg.log("Path is:", props);
+        var errmsg_generic = "\nCannot operate; crashing the server instead.";
+        var config = new Properties();
+        try {
+            config.load(new FileInputStream(props.toFile()));
+        } catch (IOException e) {
+            if (e instanceof FileNotFoundException){
+                wrn.log("A new config file needs to be created because the previous one failed to load. Error details:", e);
+                config.setProperty(KEY_HOST, "ldap");
+                config.setProperty(KEY_PORT, ""+config_port);
+                config.setProperty(KEY_USER, "cn=[some user that can read the index],ou=people,dc=example,dc=com");
+                config.setProperty(KEY_PASS, "[the password of that user]");
+                config.setProperty(KEY_SAFE, "true");
+                config.setProperty(KEY_GROUP, "ou=people,dc=example,dc=com");
+                config.setProperty(KEY_FILTER, "(&(objectclass=person)(memberOf=cn=[some group],ou=groups,dc=example,dc=com))");
+                config.setProperty(KEY_WEBSITE, "https://example.com/");
+                config.setProperty(KEY_LDAPNAME, "uid");
+                config.setProperty(KEY_MCUUID, "mcuuid");
+                config.setProperty(KEY_ERRUNKNOWN, "Something went wrong with CraftLDAP auth. THIS IS NOT YOUR FAULT, please report this error to the server admin instead!!!");
+                config.setProperty(KEY_ERRAUTH, "You're not authorized to play on this server.");
+
+                try {
+                    config.store(new FileWriter(props.toFile()), "Config file for CraftLDAP. Refer to Modrinth page or GitHub README for documentation.");
+                } catch (IOException ex) {
+                    err.log("Couldn't open the config file due to it not yet existing. Error details:", e, "\nSubsequently, attempted to create a new one. That, too however, failed. Error details:", ex, errmsg_generic);
+                    throw new RuntimeException(ex);
+                }
+            } else {
+                err.log("Couldn't open the config file, for a reason other than „it simply not being there”. Error details:", e, errmsg_generic);
+                throw new RuntimeException(e);
+            }
+        }
+        try{
+            config_port = Integer.parseInt(config.getProperty("port"));
+        } catch (NumberFormatException e) {
+            wrn.log("Couldn't read port value \""+config.getProperty(KEY_PORT)+"\" due to the following error: ", e, "\nUsing the default value of "+config_port+" instead.");
+        }
+        boolean config_safe = Boolean.parseBoolean(config.getProperty(KEY_SAFE));
+        out.log("Config obtained!");
+
+        out.log("Connecting to LDAP...");
+        LDAPConnection connection = null;
+        try (var con = new LDAPConnection(config.getProperty(KEY_HOST), config_port, config.getProperty(KEY_USER), config.getProperty(KEY_PASS))) {
+            connection = con;
+            out.log("LDAP connection established!");
+        } catch (LDAPException e) {
+            err.log("Couldn't connect to LDAP. Error details:", e);
+            if (!config_safe){
+                wrn.log("Safe-mode is OFF! That means that CraftLDAP will „fail-open”, ie. let anyone join (by skipping event handler registration) if an error occurs. Said error just occurred, and so your server is currently running without protection! This should only be used for testing or under special circumstances.");
+                return;
+            }
+        }
+
+        out.log("Registering the event handler...");
+        ServerPlayConnectionEvents.INIT.register(new JoinEventHandler(connection, config));
+        out.log("Event handler registered!");
+        out.log("CraftLDAP started!");
+
+        /*
+
+        My earlier testing session (I was trying to assess where is the best event to hook into). Kept in, because I think I'll turn it into a micro-mod at some point (I hate it how Minecraft will sometimes provide absolutely no information that there was any login attempt, only that - from apparently nowhere, and without joining prior - a player is reported to be disconnected).
+
+        ServerPlayConnectionEvents.INIT.register((ServerPlayNetworkHandler serverPlayNetworkHandler, MinecraftServer minecraftServer) -> {
+            System.out.println("Play init: " + serverPlayNetworkHandler.player.getName());
+        });
+
+        ServerPlayConnectionEvents.JOIN.register((ServerPlayNetworkHandler serverPlayNetworkHandler, PacketSender packetSender, MinecraftServer minecraftServer) -> {
+                System.out.println("Play join: " + serverPlayNetworkHandler.player.getName());
+        });
+
+        ServerLoginConnectionEvents.INIT.register((ServerLoginNetworkHandler serverLoginNetworkHandler, MinecraftServer minecraftServer) -> {
+            System.out.println("Login init: " + serverLoginNetworkHandler.getConnectionInfo());
+        });
+
+        ServerLoginConnectionEvents.QUERY_START.register((ServerLoginNetworkHandler serverLoginNetworkHandler, MinecraftServer minecraftServer, LoginPacketSender loginPacketSender, ServerLoginNetworking.LoginSynchronizer loginSynchronizer) -> {
+                System.out.println("Login query: " + serverLoginNetworkHandler.getConnectionInfo());
+        });*/
     }
 }
